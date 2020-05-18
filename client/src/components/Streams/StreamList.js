f93598d68d12e0e4f3b485005c0996d018374b7d
tyrion404 <chintan.170410116089@gmail.com>
2020-05-18 21:00:10 +0530

    Stream List and Create Stream components.

--- client/src/components/Streams/StreamList.js
+++ client/src/components/Streams/StreamList.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
 class StreamList extends Component {
+
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderAdmin = (stream) => {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
+                </div>
+            );
+        }
+    };
+
+    renderList = () => {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id} style={{ padding: '12px' }}>
+                    {this.renderAdmin(stream)}
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    };
+
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
-                StreamList
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderList()}
+                </div>
             </div>
         );
     }
 }
 
-export default StreamList;
+const mapStateToProps = (state) => {
+    return { streams: Object.values(state.streams), currentUserId: state.auth.userId };
+};
+
+export default connect(
+    mapStateToProps,
+    {
+        fetchStreams
+    }
+)(StreamList);
