782ef60fc3dae6e75859d1f1cd6dcb1b605832a8
tyrion404 <chintan.170410116089@gmail.com>
2020-05-20 16:20:49 +0530

    Live Stream works. Documentation will be added soon.

--- client/src/components/Streams/StreamShow.js
+++ client/src/components/Streams/StreamShow.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import flv from 'flv.js';
 
 import { fetchStream } from '../../actions';
 
 class StreamShow extends Component {
 
+    constructor(props) {
+        super(props);
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+        const { id } = this.props.match.params;
+        this.props.fetchStream(id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+        const { id } = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render() {
-        if (!this.props.stream || (this.props.isSignedIn === null)) {
+        if (!this.props.stream) {
             return (
                 <div className="ui active center loader"></div>
             );
         }
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls />
                 <h3>{this.props.stream.title}</h3>
                 {this.props.stream.description}
             </div>
